0fee564207ec9da5a2acfc81deb70b0a2c1f9581
Gabriel Casseb <ogabrielcasseb@gmail.com>
2022-12-23 15:33:21 -0300

    updated regex

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const openAi = require('./services/openAi');
 discordClient.on('messageCreate', async message => {
     if (message.author.bot) return;
     
-    let regex = /^Companheiro, ?/;
+    let regex = /[cC]ompanheiro,? ?/;
     if (regex.test(message.content)) {
+        console.log('Received message from ' + message.author.username);
         openAi(message.content.replace(regex, ''))
         .then(res => {
             return message.reply('Companheiro, ' + res.data.choices[0].text);
@@ -14,14 +15,5 @@ discordClient.on('messageCreate', async message => {
         .catch(err => {
             return message.reply('Companheiro, essa eu vou ter que perguntar pra Dilma');
         })
-    }
-});
-
-discordClient.on('interactionCreate', async interaction => {
-    console.log('interaction');
-    if (!interaction.isCommand()) return;
-    
-    if (interaction.commandName === 'perguntar') {
-        await openAi(interaction);
     }
 });
